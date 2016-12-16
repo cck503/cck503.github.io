133c3075a10c7e036d446047e142ff0799d5419a
cck503 <chi_chih@hotmail.com>
2016-12-16 11:44:00 +0800

    Add files via upload

--- mboard-googlead/public/js/mboard-custom.js
+++ mboard-googlead/public/js/mboard-custom.js
@@ -52,4 +52,102 @@ jQuery(document).ready(function($){
     $(".opinion").click(function() {
         $(".txt-span.fade.in").removeClass("in");
     });
+    $(".new-feature").click(function() {
+        $(this).find('.dot').addClass("stop");
+    });
+    $(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
 });
+
+
+//tour
+$(function() {
+  var $mboardtour1, duration, remaining, tour;
+  $mboardtour1 = $("#demo");
+  tour = new Tour({
+    onStart: function() {
+      return $mboardtour1.addClass("disabled", true);
+    },
+    onEnd: function() {
+      return $mboardtour1.removeClass("disabled", true);
+    },
+    backdrop: true,
+    backdropContainer: 'body',
+    backdropPadding: 20,
+    debug: true
+  });
+  duration = 5000;
+  remaining = duration;
+  tour.addSteps([
+    {
+      element: "#demo",
+      placement: "left",
+      title: "新增話題",
+      content: "點選即可快速新增話題<br><a href='L4-thread-post.html' target='_blank'>直接前往</a>"
+    },
+    {
+      element: "#usage",
+      placement: "bottom",
+      title: "自訂最愛專區",
+      content: "請點選設定即可自行設定個人化6個常用專區"
+    }/**/
+  ]);
+  tour.init();
+  tour.start();
+  /*if (tour.ended()) {
+    $('<div class="alert alert-info alert-dismissable"><button class="close" data-dismiss="alert" aria-hidden="true">&times;</button>You ended the demo tour. <a href="#" data-demo>Restart the demo tour.</a></div>').prependTo(".content").alert();
+  }*/
+  $(document).on("click", "[data-demo]", function(e) {
+    e.preventDefault();
+    if ($(this).hasClass("disabled")) {
+      return;
+    }
+    tour.restart();
+    return $(".alert").alert("close");
+  });
+  /*$("html").smoothScroll();
+  return $(".gravatar").each(function() {
+    var $this, email;
+    $this = $(this);
+    email = md5($this.data("email"));
+    return $(this).attr("src", "http://www.gravatar.com/avatar/" + email + "?s=60");
+  });*/
+});
+/*$(function() {
+  var $mboardtour2, duration, remaining, tour2;
+  $mboardtour2 = $("#usage");
+  tour2 = new Tour({
+    onStart: function() {
+      return $mboardtour2.addClass("disabled", true);
+    },
+    onEnd: function() {
+      return $mboardtour2.removeClass("disabled", true);
+    },
+    backdrop: true,
+    backdropContainer: 'body',
+    backdropPadding: 20,
+    debug: true
+  });
+  duration = 5000;
+  remaining = duration;
+  tour2.addSteps([
+    {
+      element: "#usage",
+      placement: "bottom",
+      title: "自訂最愛專區",
+      content: "請點選設定即可自行設定個人化6個常用專區"
+    }
+  ]);
+  tour2.init();
+  tour2.start();
+  
+  $(document).on("click", "[data-usage]", function(e) {
+    e.preventDefault();
+    if ($(this).hasClass("disabled")) {
+      return;
+    }
+    tour2.restart();
+    return $(".alert").alert("close");
+  });
+});*/
