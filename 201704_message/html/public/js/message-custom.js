c21fdf4ea3038be2a24b85f1163eeb9a2183b7b9
cck503 <chi_chih@hotmail.com>
2017-04-27 15:15:54 +0800

    20170427-update-01

--- 201704_message/html/public/js/message-custom.js
+++ 201704_message/html/public/js/message-custom.js
@@ -1,7 +1,11 @@
 jQuery(document).ready(function($) {
-
     $(".search-contact").on('focus blur', function() {
         $(".list-search").toggleClass('focused');
     })
-
+    $(".list-group-item").on('click touch', function () {
+        $(".chat-content").addClass("in");
+    });
+    $(".btn-back").on('click touch', function () {
+        $(".chat-content").removeClass("in");
+    });
 });
