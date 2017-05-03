a3e03eca7116dd7f1501de18414c8081b146a75c
cck503 <chi_chih@hotmail.com>
2017-05-03 17:10:00 +0800

    20170503-update-02

---
+++ 201704_message/html-20170503-2/public/js/message-custom.js
@@ -0,0 +1,25 @@
+jQuery(document).ready(function($) {
+    $(".search-contact").on('focus blur', function() {
+        $(".list-search").toggleClass('focused');
+    })
+    $(".list-group-item").on('click touch', function() {
+        $(".chat-content").addClass("in");
+    });
+    $(".btn-back").on('click touch', function() {
+        $(".chat-content").removeClass("in");
+    });
+});
+$(document)
+    .one('focus.autoExpand', 'textarea.autoExpand', function() {
+        var savedValue = this.value;
+        this.value = '';
+        this.baseScrollHeight = this.scrollHeight;
+        this.value = savedValue;
+    })
+    .on('input.autoExpand', 'textarea.autoExpand', function() {
+        var minRows = this.getAttribute('data-min-rows') | 0,
+            rows;
+        this.rows = minRows;
+        rows = Math.ceil((this.scrollHeight - this.baseScrollHeight) / 17);
+        this.rows = minRows + rows;
+    });
