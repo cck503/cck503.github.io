344b6ec44fc53b29d72e5a673f91e6f3c996ec9f
cck503 <chi_chih@hotmail.com>
2018-01-17 14:33:44 +0800

    20170117-01

--- 201709_BBH2.0/html/mobile/public/js/m-feeds-custom.js
+++ 201709_BBH2.0/html/mobile/public/js/m-feeds-custom.js
@@ -40,3 +40,16 @@ $(document).ready(function() {
         $('.leftnav-group').removeClass('active');
     });
 });
+
+
+var scroll_top_duration = 700,
+    //grab the "back to top" link
+    $back_to_top = $('.back2top');
+
+//smooth scroll to top
+$back_to_top.on('click', function(event) {
+    event.preventDefault();
+    $('body,html').animate({
+        scrollTop: 0,
+    }, scroll_top_duration);
+});
