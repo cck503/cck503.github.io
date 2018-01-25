f14be743a28ab3b628a7c5faa5b25c0fb9dd584b
cck503 <chi_chih@hotmail.com>
2018-01-25 14:38:30 +0800

    20180125-01

--- 201709_BBH2.0/html/mobile/public/js/m-header-custom.js
+++ 201709_BBH2.0/html/mobile/public/js/m-header-custom.js
@@ -3,6 +3,7 @@ var didScroll;
 var lastScrollTop = 0;
 var delta = 5;
 var navbarHeight = $('.navbar-fixed-top').outerHeight();
+var subbarHeight = $('.sub-nav').outerHeight();
 
 $(window).scroll(function(event){
     didScroll = true;
@@ -27,15 +28,27 @@ function hasScrolled() {
     if (st > lastScrollTop && st > navbarHeight){
         // Scroll Down
         $('.navbar-fixed-top').removeClass('nav-down').addClass('nav-up');
-        $('.my-sticky-element').removeClass('nav-down').addClass('nav-up');
     } else {
         // Scroll Up
         if(st + $(window).height() < $(document).height()) {
             $('.navbar-fixed-top').removeClass('nav-up').addClass('nav-down');
-            $('.my-sticky-element').removeClass('nav-up').addClass('nav-down');
         }
     }
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
     
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > subbarHeight){
+        // Scroll Down
+        $('.sub-nav').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('.sub-nav').removeClass('nav-up').addClass('nav-down');
+        }
+    }
     lastScrollTop = st;
 }
 
---
+++ 201709_BBH2.0/html/web/public/js/header-custom-2.js
@@ -0,0 +1,53 @@
+//    
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('.navbar-fixed-top').outerHeight();
+var subbarHeight = $('#covernav-fix').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('.navbar-fixed-top').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('.navbar-fixed-top').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > subbarHeight){
+        // Scroll Down
+        $('#covernav-fix').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('#covernav-fix').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+    lastScrollTop = st;
+}
